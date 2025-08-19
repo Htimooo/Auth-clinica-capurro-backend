043babf83bc9c0ad27be17c2d8feb266d42b44b9
Hugo Andrés Timote <76624227+Htimooo@users.noreply.github.com>
2025-08-19 20:38:51 -0300

    Implement password reset workflow with email

--- src/config.js
+++ src/config.js
@@ -26,5 +26,12 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME
   },
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  email: {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+    from: process.env.EMAIL_FROM
+  }
 };
